0b864fd918ecc7feff45c40d522c82c42c9a1013
Andrew Gilchrist-Scott <agilchr1@cs.swarthmore.edu>
2014-10-31 14:59:35 -0400

    Created a tree to store options for finding kfact solution

--- kfact/kfact.py
+++ kfact/kfact.py
@@ -8,6 +8,7 @@ Written by Andrew Gilchrist-Scott
 from facorize import factorize
 from math import *
 from Queue import PriorityQueue as PQ
+from copy import deepcopy
 
 def kfact(N,K):
     ideal = pow(N,1./K)
@@ -29,14 +30,42 @@ def kfact(N,K):
     # now, the hard part
     # which currently I'm stumped on an ideal not O(len(fact)^k) solution
 
-    # let first brute force this
+    # let's first do this with a tiny bit of dynamic programming: a tree
     optionsPQ = PQ()
-    optionslist = [[1]*k]
+    optionslist = [1]*k
+    Root = FactNode(optionsList)
 
-    for factor in facts:
-        newOptions = []
-        for option in optionsList:
-            for i in len(
+    nodeList = [Root]
+    for i in range(len(facts)):
+        level = i+1
+        factor = facts[i]
+        newNodeList = []
+        for node in nodeList:
+            for j in range(len(node.options)):
+                newOptions = node.options[:]
+                newOptions[j] *= factor
+                node.addChild(newOptions)
+            newNodeList += node.children
+        nodeList = newNodeList
+            
+            #TODO
+            # for every nod in the list
+            # make children for the new fact times every bucket
+            # add children to newNodeList
+
+
+class FactNode(object):
+
+    def __init__(self,options):
+        self.options = options
+        self.spread = spread(options)
+        self.children = []
+
+    def addChild(self, childOptions):
+        self.children.append(FactNode(childOptions))
+
+    def isLeaf(self):
+        return (len(self.children) == 0)
 
 
 def spread(lst):
