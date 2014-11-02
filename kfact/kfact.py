697fb546f8b52a44c632567a13b92cd9ddef1a61
Andrew Gilchrist-Scott <agilchr1@cs.swarthmore.edu>
2014-11-02 00:42:45 -0400

    Tree sctructure kfact appears to be working

--- kfact/kfact.py
+++ kfact/kfact.py
@@ -5,16 +5,16 @@ Useful for dividing a non-square area into equally sized parcels
 Written by Andrew Gilchrist-Scott
 '''
 
-from facorize import factorize
+from factorize import factorize
 from math import *
-from Queue import PriorityQueue as PQ
+#from Queue import PriorityQueue as PQ
 from copy import deepcopy
 
 def kfact(N,K):
     ideal = pow(N,1./K)
     if ideal == floor(ideal):
         # kth root of N is an int, the ideal case
-        return [ideal]*K
+        return [int(ideal)]*K
     
     # obtain the prime factorization
     facts = factorize(N)
@@ -26,18 +26,17 @@ def kfact(N,K):
         # we've too few factors, so just fill with ones
         while len(facts) < K:
             facts = [1] + facts
+        return facts
 
     # now, the hard part
     # which currently I'm stumped on an ideal not O(len(fact)^k) solution
 
     # let's first do this with a tiny bit of dynamic programming: a tree
-    optionsPQ = PQ()
-    optionslist = [1]*k
+    optionsList = [1]*K
     Root = FactNode(optionsList)
 
     nodeList = [Root]
     for i in range(len(facts)):
-        level = i+1
         factor = facts[i]
         newNodeList = []
         for node in nodeList:
@@ -48,17 +47,17 @@ def kfact(N,K):
             newNodeList += node.children
         nodeList = newNodeList
             
-            #TODO
-            # for every nod in the list
-            # make children for the new fact times every bucket
-            # add children to newNodeList
+    #we have the min spread value somewhere in the leaves of three,
+    #now we just have to retrieve it
+    (minSpread, bestFactorization) = Root.getMinSpread()
 
-
+    return bestFactorization
+    
 class FactNode(object):
 
     def __init__(self,options):
         self.options = options
-        self.spread = spread(options)
+        self.spread = self.__spread(options)
         self.children = []
 
     def addChild(self, childOptions):
@@ -67,10 +66,29 @@ class FactNode(object):
     def isLeaf(self):
         return (len(self.children) == 0)
 
+    '''
+    Recursive function to get the min spread of the leaves
+    '''
+    def getMinSpread(self):
+        if self.isLeaf():
+            return (self.spread,self.options)
+        else:
+            curMin = float("inf")
+            minOptions = []
+            min
+            for child in self.children:
+                (childSpread,childOptions) = child.getMinSpread()
+                if childSpread < curMin:
+                    curMin = childSpread
+                    minOptions = childOptions
+            return (curMin, minOptions)
 
-def spread(lst):
-    return max(lst) - min(lst)
+
+    def __spread(self, lst):
+        return max(lst) - min(lst)
     
 if __name__ == "__main__":
-    for i in range(10,50):
-        print(kfact(i,3))
+    for k in range(2,6):
+        for i in range(10,71):
+            print("The %d lowest spread factors of %d:"%(k,i))
+            print(kfact(i,k))
